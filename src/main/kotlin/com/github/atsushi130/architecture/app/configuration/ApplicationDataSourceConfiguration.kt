e23a1981f628b70cd3fc0115ba2037cff411ef00
atsushi130 <atsushi_main@outlook.jp>
2018-01-21 02:28:04 +0900

    add ApplicationDataSourceConfiguration

---
+++ src/main/kotlin/com/github/atsushi130/architecture/app/configuration/ApplicationDataSourceConfiguration.kt
@@ -0,0 +1,6 @@
+package com.github.atsushi130.architecture.app.configuration
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ApplicationDataSourceConfiguration
